95feaffae08eab12eda4fc2f194c5eec7e9e008a
Baliram <heyome9a@gmail.com>
2022-08-25 17:04:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Card from './Components/Card/Card';
+import './Sass/App.sass'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
